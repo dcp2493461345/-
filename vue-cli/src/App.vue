5793890edc760e8140eb0ff473517e2b0d9666f9
dcp2493461345 <2493461345@qq.com>
2019-12-05 20:25:18 +0800

    新增vue文件

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-link to="/index">首页</router-link>
+    <router-link to="/about">关于</router-link>
+    <router-link to="/new">新闻</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+export default{
+  
+}
+</script>
+
+<style lang="less">
+
+</style>
---
+++ vue-cli/src/views/Index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="app">
+        <h1>
+            欢迎光临
+        </h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="less" scoped>
+    .app{
+        h1{
+            color:"red";
+        }
+    }
+</style>
